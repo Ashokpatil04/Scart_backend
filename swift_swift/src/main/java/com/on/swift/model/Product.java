71a867bb524030566268b59f21a1e887d0426677
Ashok <appupatil0404@gmail.com>
2026-09-20 12:50:40 +0530

    commit

---
+++ swift_swift/src/main/java/com/on/swift/model/Product.java
@@ -0,0 +1,113 @@
+package com.on.swift.model;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonFormat.Shape;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String description;
+	private String brand;
+	private BigDecimal price;
+	private String category;
+	
+	@JsonFormat(shape = Shape.STRING, pattern="dd-MM-yyyy")
+	private Date releasedate;
+	private boolean productAvailable;
+	private int stockQuantity;
+	private String imageName;
+	private String imageType;
+    @Lob
+    private byte[] imageDate;
+	
+	public String getImageName() {
+		return imageName;
+	}
+	public void setImageName(String imageName) {
+		this.imageName = imageName;
+	}
+	public String getImageType() {
+		return imageType;
+	}
+	public void setImageType(String imageType) {
+		this.imageType = imageType;
+	}
+	public byte[] getImageDate() {
+		return imageDate;
+	}
+	public void setImageDate(byte[] imageDate) {
+		this.imageDate = imageDate;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDesc() {
+		return description;
+	}
+	public void setDesc(String desc) {
+		this.description = description;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public BigDecimal getPrice() {
+		return price;
+	}
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public Date getReleasedate() {
+		return releasedate;
+	}
+	public void setReleasedate(Date releasedate) {
+		this.releasedate = releasedate;
+	}
+	public boolean isAvailable() {
+		return productAvailable;
+	}
+	public void setAvailable(boolean productAvailable) {
+		this.productAvailable = productAvailable;
+	}
+	public int getQuantity() {
+		return stockQuantity;
+	}
+	public void setQuantity(int quantity) {
+		this.stockQuantity = quantity;
+	}
+}
